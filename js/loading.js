eb3d6e3fd4fca56482255ab6f59bc7f4acd0a9d2
Kyle B. <kylebei@icloud.com>
2025-02-26 17:23:23 +0000

    Wed, Feb 26, 2025, 9:23 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -3,9 +3,15 @@ const bg = document.querySelector('.bg')
 
 let load = 0
 
+let int = setInterval(blurring, 50)
+
 blurring()
 
 function blurring() {
     load = load + 1
+    if (load > 99){
+        clearInterval(int)
+    }
     loadText.innerText = `${load}%`
+
 }
