ce017b72fe6bd3c4eefa21fdad06e24ae95456fe
Kyle B. <kylebei@icloud.com>
2025-03-04 17:41:17 +0000

    Tue, Mar 4, 2025, 9:41 AM -08:00

--- js/expanding.js
+++ js/expanding.js
@@ -1,7 +1,14 @@
-panels = document.querySelector('.panel')
+panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
+        removeActiveClasses()
         panel.classList.add('active')
     })
 })
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
