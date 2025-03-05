0d6f333ac73b60e0efbe828739a77a1593d01a56
Kyle B. <kylebei@icloud.com>
2025-03-05 18:31:52 +0000

    Wed, Mar 5, 2025, 10:31 AM -08:00

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -5,64 +5,73 @@ const score = document.querySelector('.score')
 const status = document.querySelector('.status')
 
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 
 let gamePlaying = false
 let playerScore = 0
 let computerScore = 0
 let computerChoice = 1
 
-    rock.addEventListener('click', ()=>{
-        computerChoice = getRndInteger(1, 3)
-        console.log(computerChoice)
-        gamePlaying = true
-        if (computerChoice == 1) {
-            status.innerHTML = "Tied"
-        }
-        else if (computerChoice == 2) {
-            computerScore = computerScore + 1
-            status.innerHTML = "You lose"
-        }
-        else if (computerChoice == 3) {
-            playerScore = playerScore + 1
-            status.innerHTML = "You win"
-        }
-        score.innerHTML = `(You) ${playerScore} - (Computer) ${computerScore}`
-    })
+rock.addEventListener('click', () => {
+    rockChosen()
+})
 
-    paper.addEventListener('click', ()=>{
-        computerChoice = getRndInteger(1, 3)
-        console.log(computerChoice)
-        gamePlaying = true
-        if (computerChoice == 2) {
-            status.innerHTML = "Tied"
-        }
-        else if (computerChoice == 3) {
-            computerScore = computerScore + 1
-            status.innerHTML = "You lose"
-        }
-        else if (computerChoice == 1) {
-            playerScore = playerScore + 1
-            status.innerHTML = "You win"
-        }
-        score.innerHTML = `(You) ${playerScore} - (Computer) ${computerScore}`
-    })
+paper.addEventListener('click', () => {
+    paperChosen()
+})
 
-    scissors.addEventListener('click', ()=>{
-        computerChoice = getRndInteger(1, 3)
-        console.log(computerChoice)
-        gamePlaying = true
-        if (computerChoice == 3) {
-            status.innerHTML = "Tied"
-        }
-        else if (computerChoice == 1) {
-            computerScore = computerScore + 1
-            status.innerHTML = "You lose"
-        }
-        else if (computerChoice == 2) {
-            playerScore = playerScore + 1
-            status.innerHTML = "You win"
-        }
-        score.innerHTML = `(You) ${playerScore} - (Computer) ${computerScore}`
-    })
+scissors.addEventListener('click', () => {
+    scissorsChosen()
+})
+
+
+
+function rockChosen() {
+    computerChoice = getRndInteger(1, 3)
+    console.log(computerChoice)
+    gamePlaying = true
+    if (computerChoice == 1) {
+        status.innerHTML = "Tied"
+    } else if (computerChoice == 2) {
+        computerScore = computerScore + 1
+        status.innerHTML = "You lose"
+    } else if (computerChoice == 3) {
+        playerScore = playerScore + 1
+        status.innerHTML = "You win"
+    }
+    score.innerHTML = `(You) ${playerScore} - (Computer) ${computerScore}`
+}
+
+function paperChosen() {
+    computerChoice = getRndInteger(1, 3)
+    console.log(computerChoice)
+    gamePlaying = true
+    if (computerChoice == 2) {
+        status.innerHTML = "Tied"
+    } else if (computerChoice == 3) {
+        computerScore = computerScore + 1
+        status.innerHTML = "You lose"
+    } else if (computerChoice == 1) {
+        playerScore = playerScore + 1
+        status.innerHTML = "You win"
+    }
+    return score.innerHTML = `(You) ${playerScore} - (Computer) ${computerScore}`
+}
+
+function scissorsChosen() {
+    computerChoice = getRndInteger(1, 3)
+    console.log(computerChoice)
+    gamePlaying = true
+    if (computerChoice == 3) {
+        status.innerHTML = "Tied"
+    } else if (computerChoice == 1) {
+        computerScore = computerScore + 1
+        status.innerHTML = "You lose"
+    } else if (computerChoice == 2) {
+        playerScore = playerScore + 1
+        status.innerHTML = "You win"
+    }
+    return score.innerHTML = `(You) ${playerScore} - (Computer) ${computerScore}`
+}
