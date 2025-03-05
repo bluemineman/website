ae81242c4232e9768a454ef3e0a4ed6b919978ce
Kyle B. <kylebei@icloud.com>
2025-03-05 18:35:57 +0000

    Wed, Mar 5, 2025, 10:35 AM -08:00

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -3,6 +3,7 @@ const paper = document.getElementById('paper')
 const scissors = document.getElementById('scissors')
 const score = document.querySelector('.score')
 const status = document.querySelector('.status')
+const computerChose = document.QuerySelector('.computerChoice')
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -26,6 +27,7 @@ scissors.addEventListener('click', () => {
     scissorsChosen()
 })
 
+computerChose.innerHTML = "Computer chose: Rock"
 
 
 function rockChosen() {
@@ -34,15 +36,19 @@ function rockChosen() {
     gamePlaying = true
     if (computerChoice == 1) {
         status.innerHTML = "Tied"
+        computerChose.innerHTML = "Computer chose: Rock"
     } else if (computerChoice == 2) {
         computerScore = computerScore + 1
         status.innerHTML = "You lose"
+        computerChose.innerHTML = "Computer chose: Paper"
     } else if (computerChoice == 3) {
         playerScore = playerScore + 1
         status.innerHTML = "You win"
+        computerChose.innerHTML = "Computer chose: Scissors"
     }
     score.innerHTML = `(You) ${playerScore} - (Computer) ${computerScore}`
 }
+
 
 function paperChosen() {
     computerChoice = getRndInteger(1, 3)
@@ -50,12 +56,15 @@ function paperChosen() {
     gamePlaying = true
     if (computerChoice == 2) {
         status.innerHTML = "Tied"
+        computerChose.innerHTML = "Computer chose: Paper"
     } else if (computerChoice == 3) {
         computerScore = computerScore + 1
         status.innerHTML = "You lose"
+        computerChose.innerHTML = "Computer chose: Scissors"
     } else if (computerChoice == 1) {
         playerScore = playerScore + 1
         status.innerHTML = "You win"
+        computerChose.innerHTML = "Computer chose: Rock"
     }
     return score.innerHTML = `(You) ${playerScore} - (Computer) ${computerScore}`
 }
@@ -66,12 +75,15 @@ function scissorsChosen() {
     gamePlaying = true
     if (computerChoice == 3) {
         status.innerHTML = "Tied"
+        computerChose.innerHTML = "Computer chose: Scissors"
     } else if (computerChoice == 1) {
         computerScore = computerScore + 1
         status.innerHTML = "You lose"
+        computerChose.innerHTML = "Computer chose: Rock"
     } else if (computerChoice == 2) {
         playerScore = playerScore + 1
         status.innerHTML = "You win"
+        computerChose.innerHTML = "Computer chose: Paper"
     }
     return score.innerHTML = `(You) ${playerScore} - (Computer) ${computerScore}`
 }
