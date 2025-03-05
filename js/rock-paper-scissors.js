80b7c6a8227ce4655eaa51a562841fe9e034ca9e
Kyle B. <kylebei@icloud.com>
2025-03-05 18:10:50 +0000

    Wed, Mar 5, 2025, 10:10 AM -08:00

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,7 +1,23 @@
 const rock = document.getElementId('rock')
 const paper = document.getElementId('paper')
 const scissors = document.getElementId('scissors')
+const score = document.querySelector('.score')
+const status = document.querySelector('.status')
 
-rock.addEventListener('click' ()=> {
-    if (Math.random())
-})
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+let gamePlaying = false
+let playerScore = 0
+let computerScore = 0
+let computerChoice = 1
+while (gamePlaying = false) {
+    rock.addEventListener('click' ()=> {
+        computerChoice = getRndInteger(1, 3)
+        gamePlaying = true
+        if (computerChoice = 1) {
+            status
+        }
+    })
+}
