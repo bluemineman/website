1c2e7fb323385b9990ddc40f01d890b77c272b25
Kyle B. <kylebei@icloud.com>
2025-03-11 17:16:15 +0000

    Tue, Mar 11, 2025, 10:16 AM -07:00

--- js/sound.js
+++ js/sound.js
@@ -1,5 +1,10 @@
 const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach(sound => {
-    
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerHTML = sound
+
+
+    document.getElementById('buttons').appendChild(btn)
 })
