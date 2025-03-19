35499adc6d396e3d275dd6bc8ae3eaf7f0fa0e54
Kyle B. <kylebei@icloud.com>
2025-03-19 17:01:40 +0000

    Wed, Mar 19, 2025, 10:01 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -13,6 +13,6 @@ startBtn.addEventListener('click', () => {
 chooseInsectBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         screens[1].classList.add('up')
-        
+        const img = btn.querySelector('img')
     })
 })
