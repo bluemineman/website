441df8be797fcd14e59f08dd32e3660e53934d5b
Kyle B. <kylebei@icloud.com>
2025-03-19 17:06:35 +0000

    Wed, Mar 19, 2025, 10:06 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -6,6 +6,10 @@ const timeEl = document.getElementById('time')
 const scoreEl = document.getElementById('score')
 const message = document.getElementById('message')
 
+let seconds = 0
+let score = 0
+let selectedInsect = {}
+
 startBtn.addEventListener('click', () => {
     screens[0].classList.add('up')
 })
@@ -14,5 +18,13 @@ chooseInsectBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         screens[1].classList.add('up')
         const img = btn.querySelector('img')
+        const src = img.getAttribute('src')
+        const alt = img.getAttribute('alt')
+        selectedInsect = {src, alt}
+        startGame()
     })
 })
+
+function increaseTime() {
+    let 
+}
