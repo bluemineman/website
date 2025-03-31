c9f5b38ecbe966dacc38d5b7fa472c29bc6c0c5d
Kyle B. <kylebei@icloud.com>
2025-03-31 16:04:38 +0000

    Mon, Mar 31, 2025, 9:04 AM -07:00

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 const btn = document.getElementById('btn')
-const 
+const content = document.querySelector('.container')
 
 const list = ['running', 'paused']
 let index = 0
 btn.addEventListener('click', () =>{
-    .style.animationPlayState = list[index]
+    content.style.animationPlayState = list[index]
     index = (index + 1) % 2
 })
