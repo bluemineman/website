844835835ec164e65b051c53cbe5036d903bb52b
Kyle B. <kylebei@icloud.com>
2025-04-07 16:37:25 +0000

    Mon, Apr 7, 2025, 9:37 AM -07:00

---
+++ js/ball.js
@@ -0,0 +1,42 @@
+const ball = document.createElement('div')
+
+const ballRadius = 50
+const windowHeight = window.innerHeight
+const windowWidth = window.innerWidth
+
+let ballSpeed = 5
+let ballXPosition = windowWidth / 2 - ballRadius
+let ballXDirection = 1
+
+let ballYPosition = windowHeight / 2 - ballRadius
+let ballYDirection = 1
+
+createBall()
+
+setInterval(moveBall, 10)
+
+function moveBall() {
+    ballXPosition = ballXPosition + ballSpeed * ballXDirection
+    ball.style.left = `${ballXPosition}px`
+    if (ballXPosition < 0 || ballXPosition > windowWidth - 2 * ballRadius) {
+        ballXDirection = ballXDirection * -1
+    }
+    ballYPosition = ballYPosition + ballSpeed * ballYDirection
+    ball.style.top = `${ballYPosition}px`
+    if (ballYPosition < 0 || ballYPosition > windowHeight - 2 * ballRadius) {
+        ballYDirection = ballYDirection * -1
+    }
+}
+
+function createBall() {
+    document.body.appendChild(ball)
+    ball.style.height = `${2 * ballRadius}px`
+    ball.style.width = `${2 * ballRadius}px`
+    ball.style.borderRadius = "50%"
+    ball.style.backgroundColor = "red"
+    ball.style.position = "absolute"
+    ball.style.top = `${ballYPosition}px`
+    ball.style.left = `${ballXPosition}px`
+}
+
+
